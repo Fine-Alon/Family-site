e5f4fc132c82ee8a51f3644a0174b9726a423af2
Alon Fine <mecheniy5462@gmail.com>
2025-11-08 13:06:01 +0200

    integration HERO-UI + bilding of Header component

---
+++ src/app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next"
+import { Geist, Geist_Mono } from "next/font/google"
+import "./globals.css"
+import {Providers} from "./providers";
+import Header from "@/components/UI/Header"
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+})
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+})
+
+export const metadata: Metadata = {
+  title: "Welcome home ISA",
+  description: "It is not important part here cose it is my own app NOT for searching by bots",
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+          <Header/>
+          {children}         
+        </Providers>
+
+      </body>
+    </html>
+  )
+}
