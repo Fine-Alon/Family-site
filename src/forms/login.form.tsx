c353101594a151caae94af974d1f6d431c026e9b
Alon Fine <mecheniy5462@gmail.com>
2025-11-14 20:00:55 +0200

    made Login + Registration forms components

---
+++ src/forms/login.form.tsx
@@ -0,0 +1,83 @@
+import { useState } from "react"
+import { Form, Input, Button } from "@heroui/react"
+
+
+interface IProps {
+  onFormClose: () => void
+}
+
+export const FormLogin = ({ onFormClose }: IProps) => {
+
+  const [formData, setFormData] = useState({
+    email: '',
+    name: '',
+    password: '',
+    confirmPassword: '',
+    phone: '',
+    // agreeToTerms: false, // if you have a checkbox
+  })
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log('Form submitted successfully', formData)
+    onFormClose()
+  }
+
+  const validatePassword = (password: string) => {
+    if (typeof password !== 'string' || password.trim() === '') return false
+    return true
+  }
+
+  return (
+    <Form className="w-full max-w-xs" onSubmit={handleSubmit}>
+      <Input
+        isRequired
+        errorMessage="Please enter a valid email"
+        label="Email"
+        labelPlacement="outside"
+        value={formData.email}
+        name="email"
+        placeholder="Enter your email"
+        type="email"
+        classNames={{
+          inputWrapper: 'bg-default-100',
+          input: 'text-sm focus:out-none'
+        }}
+        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        validate={(value) => value ? null : "Email must be field"}
+      />
+
+      <Input
+        isRequired
+        errorMessage="Please enter a valid password"
+        label="password"
+        labelPlacement="outside"
+        name="password"
+        placeholder="Enter password"
+        type="password"
+        classNames={{
+          inputWrapper: "bg-default-100",
+          input: 'text-sm focus:out-none'
+        }}
+        onChange={(e) => { setFormData({ ...formData, password: e.target.value }) }}
+        validate={(value) => value ? null : "It's too short to be password! At least 6 symbols"}
+      />
+
+      <div>
+        <Button variant="light" onPress={onFormClose} >
+          Cancel
+        </Button>
+        <Button color="primary" type="submit" variant="bordered">
+          Confirm and sent
+        </Button>
+      </div>
+      {formData && (
+        <div className="text-small text-default-500">
+          You sent data as: <code>{JSON.stringify(formData)}</code>
+        </div>
+      )}
+    </Form>
+  )
+}
+
+export default FormLogin
--- src/forms/registration.form.tsx
+++ src/forms/registration.form.tsx
@@ -6,7 +6,7 @@ interface IProps {
   onFormClose: () => void
 }
 
-export const FormRegistration = ({onFormClose}:IProps) => {
+export const FormRegistration = ({ onFormClose }: IProps) => {
 
   const [formData, setFormData] = useState({
     email: '',
@@ -20,16 +20,23 @@ export const FormRegistration = ({onFormClose}:IProps) => {
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     console.log('Form submitted successfully', formData)
-
-    const data = e.currentTarget
     onFormClose()
   }
 
   const validateEmail = (email: string) => {
     if (typeof email !== 'string' || email.trim() === '') return false
-
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     return emailRegex.test(email.trim())
+  }
+
+  const validateName = (name: string) => {
+    if (typeof name !== 'string' || name.trim() === '') return false
+    const nameRegex = /^[\p{L}\s\-']+$/u
+    return nameRegex.test(name.trim())
+  }
+  const validatePassword = (password: string) => {
+    if (typeof password !== 'string' || password.trim() === '') return false
+    return true
   }
 
   return (
@@ -49,9 +56,9 @@ export const FormRegistration = ({onFormClose}:IProps) => {
         }}
         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
         validate={(value) => {
-          if (value.length < 2) return "Email must be field"
+          if (!value) return "Email must be field"
           if (!validateEmail(value)) return "Email not correct"
-          return value === "admin" ? "Nice try!" : null
+          return null
         }}
       />
       <Input
@@ -62,16 +69,69 @@ export const FormRegistration = ({onFormClose}:IProps) => {
         name="name"
         placeholder="Enter your name"
         type="text"
+        classNames={{
+          inputWrapper: "bg-default-100",
+          input: 'text-sm focus:out-none'
+        }}
+        onChange={(e) => { setFormData({ ...formData, name: e.target.value }) }}
+        validate={(value) => {
+          if (value.length < 2) return "It's too short to be name"
+          if (!validateName(value)) return "Name is not correct!"
+          return null
+        }}
       />
-      <Button type="submit" variant="bordered">
-        Confirm and sent
-      </Button>
-      {submitted && (
+      <Input
+        isRequired
+        errorMessage="Please enter a valid password"
+        label="password"
+        labelPlacement="outside"
+        name="password"
+        placeholder="Enter password"
+        type="password"
+        classNames={{
+          inputWrapper: "bg-default-100",
+          input: 'text-sm focus:out-none'
+        }}
+        onChange={(e) => { setFormData({ ...formData, password: e.target.value }) }}
+        validate={(value) => {
+          if (value.length < 6) return "It's too short to be password! At least 6 symbols"
+          if (!validatePassword(value)) return "Password is not correct!"
+          return null
+        }}
+      />
+      <Input
+        required
+        errorMessage="passwords are NOT the same"
+        name="confirmPassword"
+        label="confirm password"
+        labelPlacement="outside"
+        type="password"
+        placeholder="repeat password"
+        classNames={{
+          inputWrapper: 'bg-default-100',
+          input: 'text-sm focus:outline-none'
+        }}
+        onChange={(e) => { setFormData({ ...formData, confirmPassword: e.target.value }) }}
+        validate={(value) => {
+          if (value !== formData.password) return 'passwords are NOT the same'
+          return null
+        }}
+      />
+      <div>
+        <Button variant="light" onPress={onFormClose} >
+          Cancel
+        </Button>
+        <Button color="primary" type="submit" variant="bordered">
+          Confirm and sent
+        </Button>
+      </div>
+      {formData && (
         <div className="text-small text-default-500">
-          You sent data as: <code>{JSON.stringify(submitted)}</code>
+          You sent data as: <code>{JSON.stringify(formData)}</code>
         </div>
       )}
     </Form>
   )
 }
 
+export default FormRegistration
