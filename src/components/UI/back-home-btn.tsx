b5a7884ed7754b141e0c815c2dc57f3453f5cec1
Alon Fine <mecheniy5462@gmail.com>
2025-12-23 13:07:58 +0200

    made proxy + restricted page component

--- src/components/UI/back-home-btn.tsx
+++ src/components/UI/back-home-btn.tsx
@@ -1,15 +1,15 @@
 import Link from "next/link"
 
-const BackHomeBtn = ()=>{
+const BackHomeBtn = () => {
   return <Link
-            href="/"
-            className="inline-flex items-center px-6 py-3 border border-transparent 
-            text-base font-medium rounded-md shadow-sm text-white bg-indigo-800
-             hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2
-             focus:ring-indigo-500 transition-colors"
-          >
-            ← Go Back Home
-          </Link>
+    href="/"
+    className="inline-flex items-center px-6 py-3 border border-transparent 
+        text-base font-medium rounded-md shadow-sm text-white bg-violet-600
+        hover:bg-violet-500 focus:outline-none focus:ring-2 focus:ring-offset-2
+        focus:ring-violet-500 transition-colors"
+  >
+    ← Go Back Home
+  </Link>
 }
 
 export default BackHomeBtn
--- src/proxy.ts
+++ src/proxy.ts
@@ -1,12 +1,24 @@
-"use server"
-
+import { getToken } from 'next-auth/jwt'
 import { NextResponse, NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/no-access', request.url))
+
+// This middleware/proxy function can be marked `async` if using `await` inside
+export const proxy = async (request: NextRequest) => {
+
+  // authjs.session-token
+  const sessionToken = await getToken({ req: request, secret: process.env.AUTH_SECRET })
+
+
+  console.log('sessionToken :  ', sessionToken)
+
+
+  if (!sessionToken) {
+    return NextResponse.redirect(new URL('/no-access', request.url))
+  }
+
+  // if OK pass req forward to app
+  return NextResponse.next()
 }
- 
+
 export const config = {
   matcher: '/tasks/:path*',
 }
