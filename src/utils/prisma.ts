60884ea8f5c9755637711253a4cf2283ee0cc3e0
Alon Fine <mecheniy5462@gmail.com>
2025-12-27 15:24:08 +0200

    fix (prisma): add dotenv packeg to resolve conflict (generate Prisma) by Vercel

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,6 +1,6 @@
 // Single Ton pattern
 
-import { PrismaPg } from '@prisma/adapter-pg'
+// import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from "@prisma/extension-accelerate"
 
@@ -9,13 +9,13 @@ const globalForPrisma = global as unknown as {
   prisma: PrismaClient
 }
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+// use for local DB connection
+// const adapter = new PrismaPg({
+//   connectionString: process.env.DATABASE_URL,
+// })
 
 const prisma = globalForPrisma.prisma || new PrismaClient({
   accelerateUrl: process.env.DATABASE_URL,
-  adapter,
 }).$extends(withAccelerate())
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
